084866e38a00c761304d43e7be0755908bcacde4
Its Me <evilrakion@gmail.com>
2018-10-09 17:10:40 +0400

    first commit

---
+++ app/src/main/java/com/example/dudetime/cia4003_week7_activity10_firebasetutorials/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.dudetime.cia4003_week7_activity10_firebasetutorials;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
